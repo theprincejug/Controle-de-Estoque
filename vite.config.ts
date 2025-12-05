b7f180ab7c847fa5e667f754446cf84c7396c898
Sandro Vitor de Almeida Santos <94575814+theprincejug@users.noreply.github.com>
2025-12-05 14:43:17 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [
+      '4j2ypr-5174.csb.app',
+      '.csb.app', // Permite todos os subdomínios do CodeSandbox
+    ],
+  },
 })
 
